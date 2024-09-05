c9a70e730f5ca787a32489440d813f119f2591d8
choi <rosoa0475@gmail.com>
2024-09-05 15:19:38 +0900

    [FEAT] initialize project

---
+++ dodream/src/test/java/com/hhhh/dodream/DodreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhhh.dodream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DodreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
