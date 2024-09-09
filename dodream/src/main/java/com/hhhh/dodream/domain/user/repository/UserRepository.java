5cdb051c4cb59c2674d13ab0b258548083996108
Chanho lee <devchlee12@gmail.com>
2024-09-09 13:55:43 +0900

    [FEAT] 유저 리포지토리 기능 추가

--- dodream/src/main/java/com/hhhh/dodream/domain/user/repository/UserRepository.java
+++ dodream/src/main/java/com/hhhh/dodream/domain/user/repository/UserRepository.java
@@ -7,4 +7,9 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByLoginId(String loginId);
+
+    boolean existsByEmail(String email);
+    boolean existsByNickname(String nickname);
+
+    boolean existsByLoginId(String loginId);
 }
