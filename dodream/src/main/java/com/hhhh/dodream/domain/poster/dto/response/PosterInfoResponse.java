6b469bb2f753bf5cc088d78cbef88f07d14342b9
Chanho lee <devchlee12@gmail.com>
2024-10-19 21:33:03 +0900

    [FEAT] 포스터 관련 DTO 추가

--- dodream/src/main/java/com/hhhh/dodream/domain/poster/dto/response/PosterInfoResponse.java
+++ dodream/src/main/java/com/hhhh/dodream/domain/poster/dto/response/PosterInfoResponse.java
@@ -1,13 +1,20 @@
 package com.hhhh.dodream.domain.poster.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.hhhh.dodream.domain.poster.entity.PosterInfo;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.geo.Point;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@JsonInclude(Include.NON_NULL)
 public class PosterInfoResponse {
     private String id;
     private String images;
