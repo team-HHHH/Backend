04fdc314fd9a93ea1d285d1bfbc05cc941c71db0
Chanho lee <devchlee12@gmail.com>
2024-11-14 17:37:05 +0900

    [FIX] ObjectMapper에 Java8타입 직렬화/역직렬화 기능 추가

--- dodream/src/main/java/com/hhhh/dodream/global/config/BeanConfig.java
+++ dodream/src/main/java/com/hhhh/dodream/global/config/BeanConfig.java
@@ -1,6 +1,7 @@
 package com.hhhh.dodream.global.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.springframework.context.annotation.Bean;
@@ -11,17 +12,19 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class BeanConfig {
 
     @Bean
-    public BCryptPasswordEncoder passwordEncoder(){
+    public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public JPAQueryFactory jpaQueryFactory(EntityManager em){
+    public JPAQueryFactory jpaQueryFactory(EntityManager em) {
         return new JPAQueryFactory(em);
     }
 
     @Bean
-    public ObjectMapper objectMapper(){
-        return new ObjectMapper();
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper;
     }
 }
